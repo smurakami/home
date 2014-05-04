8b0b0be84127cc02195fb64f23dd1f4af2240dd7
s_murakami <s.murakami@conol.jp>
2014-05-05 07:14:46 +0900

    Modified top/home.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Homepage::Application.routes.draw do
-  get "top/home"
+  root "top#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +40,7 @@ Homepage::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
